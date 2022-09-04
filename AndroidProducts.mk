80c3fa12cae411fe6a8acab8c28bc9f89fce665d
Eyüp DURAK <dkey5@pm.me>
2022-09-04 17:32:11 +0300

    j7elte: attention error

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,7 @@
 LOCAL_PATH := device/samsung/j7elte
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/lineage_j7elte.mk
+    $(LOCAL_PATH)/lineage_j7elte.mk \
     $(LOCAL_PATH)/rr_j7elte.mk
 
 COMMON_LUNCH_CHOICES := \
